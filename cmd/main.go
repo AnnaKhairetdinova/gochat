828f6231fe31845865d615ddffeaa124fd5c366e
a.khairetdinova <AnnKhairetdinova@yandex.ru>
2026-08-17 19:33:48 +0500

    add db: Connect and runMigrations methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {}
+
+func Connect(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, dsn)
+	pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		fmt.Errorf("connect db: %w", err)
+	}
+
+	return pool, nil
+}
+
+func RunMigrations(dsn string) error {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"pgx",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("миграции уже накатаны")
+		} else {
+			return err
+		}
+	}
+
+	return err
+}
