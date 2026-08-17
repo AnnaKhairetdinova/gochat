f9fe70986a5aa671152bfb91176532af33675209
a.khairetdinova <AnnKhairetdinova@yandex.ru>
2026-08-17 19:39:24 +0500

    add domain: Message struct and MessageType constants

--- internal/domain/message.go
+++ internal/domain/message.go
@@ -1,1 +1,27 @@
 package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type MessageType string
+
+const (
+	TypeMessage MessageType = "message"
+	TypeSystem  MessageType = "system"
+)
+
+type Message struct {
+	UUID      uuid.UUID   `json:"uuid"`
+	Room      string      `json:"room"`
+	Username  string      `json:"user"`
+	Text      string      `json:"text"`
+	Type      MessageType `json:"type"`
+	CreatedAt time.Time   `json:"timestamp"`
+}
+
+type IncomingMessage struct {
+	Text string `json:"text"`
+}
